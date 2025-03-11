b6246ce3c43fedbf066aad1b492fae49106581b3
M-Marxell <marcellmoravetz@gmail.com>
2025-03-11 07:56:29 +0100

    1. tétel elkészült

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
+<?php
+$tombBe = array("B","B");
+$tombki = array("","","","");
+$betu="A";
+$indexMax = count($tombBe)-1;
+for($i=0;$i<=$indexMax;$i++)
+{
+    $tombki[$i]=$tombBe[$i].$betu;
+    $tombki[$i]=$tombki[$i].$tombki[$i];
+}
 
+print_r($tombBe); print"<br>";
+print_r($tombki); print"<br>";
